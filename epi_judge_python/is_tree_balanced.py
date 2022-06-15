89d37716b155fa741cb3f0a7eb795dec44d83bd6
lennardong <lennard.ong@gmail.com>
2022-06-15 22:16:14 +0800

    WIP. Understanding input format.

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,9 +1,25 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+import collections
 
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+    print(
+        f'''
+        Tree: {tree}
+        Data: {tree.data}
+        Left: {tree.left}
+        Right: {tree.right}''')
+
+    balanced_status_w_height = collections.namedtuple(
+        'balanced_status', ['balanced' 'height'])
+
+    depth = 0 
+    while tree: 
+        print (f'\ndepth:  {depth}\ndata:   {tree.data}\nleft:   {tree.left}')
+        tree = tree.left
+        depth += 1 
+
     return True
 
 
